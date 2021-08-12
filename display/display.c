73821e930ed08d181ac8aff33f5440ae03adb30e
Jojojojojojo <jojojojojojo@pc-27.home>
2021-08-12 17:40:53 +0200

    Changing way to print pxl

--- display/display.c
+++ display/display.c
@@ -6,7 +6,7 @@
 /*   By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 21:47:31 by jcluzet           #+#    #+#             */
-/*   Updated: 2021/08/12 17:39:29 by jcluzet          ###   ########.fr       */
+/*   Updated: 2021/08/12 17:40:49 by jcluzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	display(t_long *sl)
 	render(sl);
 	write(1, "\033[32m[ so_long launch successfully ]\033[00m\n", 43);
 	write(1, "        MOVE : 0", 16);
+	write(1, "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b", 19);
 	pos_player(sl);
 	mlx_put_image_to_window(sl->mlx_ptr,
 		sl->mlx_win, sl->img, 0, 0);
@@ -62,7 +63,7 @@ int	render(t_long *sl)
 		while (x < sl->x)
 		{
 			sl->casein++;
-			write(1, "\b\b\b\b\b\b", 7);
+			write(1, "\b\b\b\b\b\b\n\b\b\b\b\b\b\b\b\b\b\b", 19);
 			ft_putnbr(sl->casein / sl->casetotal * 100);
 			write(1, "% \n", 1);
 			gettextnum(x, y, sl);
@@ -72,7 +73,6 @@ int	render(t_long *sl)
 		x = 0;
 		y++;
 	}
-	write(1, "\b\b\b\b\b\b", 7);
 	return (0);
 }
 
