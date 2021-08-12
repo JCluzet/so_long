494580e40229d7bb65e53336346e77ff4498591a
Jojojojojojo <jojojojojojo@pc-27.home>
2021-08-12 17:48:04 +0200

    fix linux

--- display/moveplayer.c
+++ display/moveplayer.c
@@ -6,7 +6,7 @@
 /*   By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 01:10:00 by jcluzet           #+#    #+#             */
-/*   Updated: 2021/08/12 17:28:02 by jcluzet          ###   ########.fr       */
+/*   Updated: 2021/08/12 17:48:03 by jcluzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	displaymove(t_long *sl, int nb)
 	if (nb == 3)
 		sl->player_x++;
 	sl->move++;
-	write(1, "        MOVE : ", 16);
+	write(1, "MOVE : ", 16);
 	ft_putnbr(sl->move);
 	checkaremove(sl);
 	return (0);
